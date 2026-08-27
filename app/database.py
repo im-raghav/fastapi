0074099a797b7f2b6526adba7fb2cfb3aaac9c44
im-raghav <itsmeraghuvanshi@gmail.com>
2026-08-27 21:45:58 +0530

    neon db

--- app/database.py
+++ app/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
 # DATABASE_URL = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/fastapi"
+# DATABASE_URL = "postgresql://postgres:password@localhost:5432/fastapi"
+DATABASE_URL = "postgresql://neondb_owner:npg_sJUVpt2MYRX7@ep-withered-glitter-azup17cx-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
